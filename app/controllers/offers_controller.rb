2d8b6b4696d02713c96ccf4e851a125b8b8b3285
Ryan Chase <in.rchase@gmail.com>
2023-03-24 19:50:51 -0700

    add offers and users controller, update gemfile

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,17 @@
 class OffersController < ApplicationController
+  before_action :require_user, only: [:index, :show]
+
+  def index
+    offers = Offer.all.select { |offer| offer.eligible?(current_user) }
+    render json: offers, status: :ok
+  end
+
+  def show
+    offer = Offer.find_by(id: params[:id])
+    if offer.nil?
+      render json: { errors: 'Offer not found' }, status: :not_found
+      return
+    end
+    render json: offer, status: :ok
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def show
+    if current_user.nil?
+      render json: { errors: 'User not logged in' }, status: :unauthorized
+      return
+    end
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.user_id
+      render json: { status: 'success', message: "Welcome to the site, #{user_params[:first_name]}!"}, status: :created
+    else
+      render json: { status: 'error', errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :first_name, :last_name, :birthdate, :gender, :password, :password_confirmation)
+
 end
