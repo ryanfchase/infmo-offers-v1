b6b12fbe5a0b3c4ff464e6031a184344c02a6688
Ryan Chase <in.rchase@gmail.com>
2023-03-24 18:36:03 -0700

    basic user_offer controller implementation

--- app/controllers/user_offers_controller.rb
+++ app/controllers/user_offers_controller.rb
@@ -1,2 +1,24 @@
 class UserOffersController < ApplicationController
+  def create
+    # First validate that the offer exists
+    @offer = Offer.find_by(id: params[:id])
+    if @offer.nil?
+      render json: { errors: 'Offer not found' }, status: :not_found
+      return
+    end
+
+    # Then validate that the user exists
+    unless @offer.eligible?(current_user)
+      render json: { errors: 'User not eligible' }, status: :unauthorized
+      return
+    end
+
+    # Then attempt to create the user offer
+    @user_offer = current_user.user_offers.build(:offer_id => @offer.id)
+    if @user_offer.save
+      render json: @user_offer, status: :created
+    else
+      render json: @user_offer.errors, status: :unprocessable_entity
+    end
+  end
 end
